350fd8f13342dae48603e5fe901ac4d8eb672dea
liko006 <67811331+liko006@users.noreply.github.com>
2020-08-15 18:30:18 +0900

    Create ozone_sklearn.py

---
+++ ozone_sklearn.py
@@ -0,0 +1,17 @@
+from sklearn import linear_model
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('./data/ozone.csv', sep=',')
+df = df.dropna(how='any')
+
+X = df[['Solar.R','Wind','Temp']]
+y = df['Ozone']
+
+lm = linear_model.LinearRegression()
+lm.fit(X,y)
+print('W의 값: {}'.format(lm.coef_))
+print('intercept : {}'.format(lm.intercept_))
+
+predictions = lm.predict([[190.0, 7.4, 67.0]])
+print('예측값: {}'.format(predictions))
